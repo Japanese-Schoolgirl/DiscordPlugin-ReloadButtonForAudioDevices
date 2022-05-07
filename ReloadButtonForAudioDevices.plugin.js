fff3fc1c2430229524a761bfcbecc47b31a541be
Lisa <kawaiipurrsona@gmail.com>
2022-05-08 01:01:17 +0300

    Slightly corrected noticed bugs

--- ReloadButtonForAudioDevices.plugin.js
+++ ReloadButtonForAudioDevices.plugin.js
@@ -17,57 +17,62 @@ class ReloadButtonForAudioDevices {
 	{
 		const PluginName = this.getName();
 		const PluginNameAsID = this.getNameAsID();
+		// Editable variables //
+		const clicksDelay = 400;
+		const attemptsLimit = 5;
+		////////////////////////
 		function GetLanguage()
 		{
-			if(document.querySelector('[aria-label*="Пользовательская область"]')) { return 'ru'; }
-			if(document.querySelector('[aria-label*="User area"]')) { return 'en'; }
-			else { return console.error(`Plugin ${PluginName} doesn't support your current Discord language. Currently this spaghetti code works only with English and Russian languages :(`); }
+			let errorMsg = `Plugin ${PluginName} doesn't support your current Discord language. Currently this spaghetti code works only with English and Russian languages :(`;
+			let returnLang = navigator.language == "ru" ? "ru" : navigator.language == "en-US" ? "en-US" : null;
+			if(!returnLang) { console.error(errorMsg); window.alert(errorMsg); }
+			return returnLang
 		}
-		function ReloadDiscordAudio()
+		function SyncTimeout(func, ms)
 		{
+			return new Promise(resolve => setTimeout(() => { resolve(); func(); }, ms));
+		}
+		async function ReloadDiscordAudio()
+		{
+			async function ClickOrWait(queryParam, attempt = 1)
+			{
+				if(attempt >= attemptsLimit) { return };
+				if(!document.querySelector(queryParam)) { return await SyncTimeout(()=> { ClickOrWait(queryParam, attempt+1) }, clicksDelay) }
+				document.querySelector(queryParam).click();
+			}
+			async function DevicesClick(Device_Label, attempt = 1)
+			{
+				if(!Device_Label) { return console.error("Where is my parameter?"); }
+				if(attempt >= attemptsLimit) { return }
+
+				try
+				{
+					let DeviceSetting = [...document.querySelectorAll('h5')].filter(el => el.innerText === Device_Label)[0].parentElement.querySelector('span[class*="deviceOptionLabel-"]');
+					DeviceSetting.click();
+
+					let ConfirmDevice = document.querySelector('svg[class*="selectedIcon-"]').parentElement.querySelector('span[class*="deviceOptionLabel-"]');
+					ConfirmDevice.click();
+				} catch(err) { return await SyncTimeout(()=> { DevicesClick(Device_Label, attempt+1) }, clicksDelay); }
+			}
 			let Label_UserSettings = DiscordLang == "ru" ? "Настройки пользователя" : "User Settings";
-			let Label_InputDevice =  DiscordLang == "ru" ? "УСТРОЙСТВО ВВОДА" : "INPUT DEVICE";
+			let Label_InputDevice = DiscordLang == "ru" ? "УСТРОЙСТВО ВВОДА" : "INPUT DEVICE";
 			let Label_OutputDevice = DiscordLang == "ru" ? "УСТРОЙСТВО ВЫВОДА": "OUTPUT DEVICE";
 			let Label_Close = DiscordLang == "ru" ? "Закрыть" : "Close";
 
-			let UserSettings = document.querySelector(`button[aria-label*="${Label_UserSettings}"]`);
-			UserSettings.click();
+			let UserSettings = `button[aria-label*="${Label_UserSettings}"]`;
+			await ClickOrWait(UserSettings);
 
-			let VoicePanelOption = document.querySelector('div[aria-controls*="voice-&-video-tab"]');
-			VoicePanelOption.click();
+			let VoicePanelOption = 'div[aria-controls*="voice-&-video-tab"]';
+			await ClickOrWait(VoicePanelOption);
 
-			function DevicesClick(Device_Label)
-			{
-				if(!Device_Label) { return console.error("Where is my parameter?"); }
+			await DevicesClick(Label_InputDevice);
+			await DevicesClick(Label_OutputDevice);
 
-				let DeviceSetting = [...document.querySelectorAll('h5')].filter(el => el.innerText === Device_Label)[0].parentElement.querySelector('span[class*="deviceOptionLabel-"]');
-				DeviceSetting.click();
-
-				let ConfirmDevice = document.querySelector('svg[class*="selectedIcon-"]').parentElement.querySelector('span[class*="deviceOptionLabel-"]');
-				ConfirmDevice.click();
-			}
-			function CloseSettings()
-			{
-				let CloseSettingsButton = document.querySelector(`div[class*="closeButton-"][aria-label="${Label_Close}"][role="button"]`);
-				CloseSettingsButton.click();
-			}
-			setTimeout(()=>
-			{
-				DevicesClick(Label_InputDevice);
-				setTimeout(()=>
-				{
-					DevicesClick(Label_OutputDevice);
-					CloseSettings();
-				}, 800);
-			}, 800);
+			let CloseSettingsButton = `div[class*="closeButton-"][aria-label="${Label_Close}"][role="button"]`;
+			await ClickOrWait(CloseSettingsButton);
 		}
-		function AddAudioReloadButton()
+		function AddAudioReloadButton(DiscordUserPanel)
 		{
-			let PluginButtonText = DiscordLang == "ru" ? "Перезагрузить аудио девайсы" : "Reload audio devices";
-			let Label_UserPanel = DiscordLang == "ru" ? "Пользовательская область" : "User area";
-			//let Label_FAQ = 'Помощь'; let Label_LinkFAQ = 'https://support.discord.com'; let DiscordHelpButton = document.querySelector(`a[href*="${Label_LinkFAQ}"] div[aria-label*="${Label_FAQ}"]`);
-
-			let DiscordUserPanel = document.querySelector(`section[aria-label*="${Label_UserPanel}"]`);
 			if(!document.getElementById(PluginNameAsID))
 			{
 				let PluginButtonStyle = `color: var(--interactive-active); background-color: var(--background-tertiary); font-size: 14px; width: 100%; padding: 5px;`;
@@ -75,15 +80,29 @@ class ReloadButtonForAudioDevices {
 				DiscordUserPanel.children[1].insertAdjacentHTML('beforebegin', PluginButtonCode);
 				document.getElementById(PluginNameAsID).addEventListener('click', ReloadDiscordAudio);
 			}
-			console.log(`Plugin ${PluginName} loading complete`);
 		}
 		const DiscordLang = GetLanguage();
-		if(DiscordLang) { console.log(`Plugin ${PluginName} loading...`); setTimeout(AddAudioReloadButton, 10000); } // What a shame way to make it
+		const PluginButtonText = DiscordLang == "ru" ? "Перезагрузить аудио девайсы" : "Reload audio devices";
+		const Label_UserPanel = DiscordLang == "ru" ? "Пользовательская область" : "User area";
+
+		// Launch
+		this.ReloadPluginObserver = new MutationObserver((mutations) =>
+		{
+			if(document.getElementById(PluginNameAsID)) { return }
+			let DiscordUserPanel = document.querySelector(`section[aria-label*="${Label_UserPanel}"]`);
+			mutations.forEach((el) =>
+			{
+				if(!DiscordUserPanel) { return }
+				else { AddAudioReloadButton(DiscordUserPanel); }
+			});
+		})
+		if(DiscordLang) { this.ReloadPluginObserver.observe(document.body, { subtree: true, childList: true }); }
 	}
 
 	stop()
 	{
 		const PluginNameAsID = this.getNameAsID();
+		this.ReloadPluginObserver.disconnect();
 		if(document.getElementById(PluginNameAsID)) { document.getElementById(PluginNameAsID).remove(); }
 	}
 
